a3850ee2d0cdb00a7675c454fbf7adb3daf7540e
bhuvanleopard <cbhuvan08@gmail.com>
2025-11-06 09:20:59 +0530

    .

--- dist/app/auth/controllers/login.js
+++ dist/app/auth/controllers/login.js
@@ -8,16 +8,18 @@ export const LoginController = (model) => {
             const isPresent = await model.findOne({ email });
             if (!isPresent)
                 return res.status(400).send({ msg: "invalid try" });
-            bcrypt.compare(password, isPresent.password, (err, isAuth) => {
-                if (err || !isAuth) {
-                    return res.status(400).json({ msg: "failed" });
-                }
-            });
-            const token = jwt.sign({
-                userId: isPresent._id,
-                email: isPresent.email
-            }, process.env.JWT_SECRET, { expiresIn: '2h' });
-            return res.status(200).json({ msg: "successful", token });
+            
+            const match = await bcrypt.compare(password, isPresent.password);
+
+            if(match){
+
+                const token = jwt.sign({
+                    userId: isPresent._id,
+                    email: isPresent.email
+                }, process.env.JWT_SECRET, { expiresIn: '2h' });
+                return res.status(200).json({ msg: "successful", token });
+            }
+
         }
         catch (err) {
             return res.status(400).json({ msg: "failed" });
